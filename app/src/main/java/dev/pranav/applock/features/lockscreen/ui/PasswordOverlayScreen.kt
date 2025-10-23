d442ed1f911f202e08baafa352951b16ddcb5141
invoke <purwarpranav80@gmail.com>
2025-10-23 04:29:31 -0400

    Slightly increase button sizes

--- app/src/main/java/dev/pranav/applock/features/lockscreen/ui/PasswordOverlayScreen.kt
+++ app/src/main/java/dev/pranav/applock/features/lockscreen/ui/PasswordOverlayScreen.kt
@@ -684,7 +684,7 @@ fun KeypadSection(
             } else {
                 val availableWidth = screenWidthDp - (horizontalPadding * 2)
                 val totalSpacing = buttonSpacing * 2
-                (availableWidth - totalSpacing) / 4f
+                (availableWidth - totalSpacing) / 3.5f
             }
         }
 
