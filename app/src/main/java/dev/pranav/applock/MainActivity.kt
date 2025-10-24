fb70b8ddf942668c1d5248d1119c6924b4c16f60
Pranav Purwar <purwarpranav80@gmail.com>
2025-10-24 18:03:22 +0530

    Fix items hidden under nav bar (#94)

--- app/src/main/java/dev/pranav/applock/MainActivity.kt
+++ app/src/main/java/dev/pranav/applock/MainActivity.kt
@@ -3,11 +3,6 @@ package dev.pranav.applock
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.Modifier
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LifecycleEventEffect
@@ -29,22 +24,16 @@ class MainActivity : FragmentActivity() {
 
         setContent {
             AppLockTheme {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colorScheme.background)
-                ) {
-                    val navController = rememberNavController()
-                    val startDestination = navigationManager.determineStartDestination()
+                val navController = rememberNavController()
+                val startDestination = navigationManager.determineStartDestination()
 
-                    AppNavHost(
-                        navController = navController,
-                        startDestination = startDestination
-                    )
+                AppNavHost(
+                    navController = navController,
+                    startDestination = startDestination
+                )
 
-                    LifecycleEventEffect(Lifecycle.Event.ON_RESUME) {
-                        handleOnResume(navController)
-                    }
+                LifecycleEventEffect(Lifecycle.Event.ON_RESUME) {
+                    handleOnResume(navController)
                 }
             }
         }
